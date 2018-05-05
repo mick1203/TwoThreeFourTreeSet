ab7a17ea10f01e5eb118cbae1db894b4f1e42599
Michael Burgstaller <trial@michaelburgstaller.com>
2018-05-05 19:21:27 +0200

    Implemented get(), contains(), size(), first(), last(), height(), comparator()

--- src/two-three-four-set/src/swe4/collections/TwoThreeFourSet.java
+++ src/two-three-four-set/src/swe4/collections/TwoThreeFourSet.java
@@ -11,68 +11,147 @@ import java.util.Iterator;
  * @version 1.0
  * @since   1.0
  */
-public class TwoThreeFourSet<T> implements SortedTreeSet<T> {
+public class TwoThreeFourSet<T> 
+    implements SortedTreeSet<T> {
     
+    /**
+     * Array of Node objects, can contain up to three subtrees as
+     * specified in the instructions
+     * 
+     */
+    private TwoThreeFourSet[] subtrees;
+
+    /**
+     * Array of T objects, specifying the value that separates two
+     * subtrees
+     */
+    private T[] landmarks;
+
+    /**
+     * Number of landmarks in the object
+     */
+    private int landmarkCount;
+
+    /**
+     * A comparator object for the type T, can be null
+     */
+    private Comparator<T> c = null;
+
+    /**
+     * Compare function wrapper, allows c to be null
+     * @param   lhs Object to be compared
+     * @param   rhs Object the first one is compared to
+     * @return  Returns a negative number, 0 or a positive number
+     * depending on the first object being less than, equal, or
+     * greater than the second one.
+     */
     @SuppressWarnings("unchecked")
-    private class Node<T> {
-        /**
-         * Array of subtrees, can contain up to three subtrees as
-         * specified in the instructions
-         * 
-         */
-        private Node[]  subtrees    = new Node[4];
-        private T[]     landmarks   = (T[]) new Object[3];
-
-        public Node() {
-
-        };
-
+    private int compare(T lhs, T rhs) {
+        if (c == null) {
+            // assume T implements comparable
+            return ((Comparable<T>) lhs).compareTo(rhs);
+        } else {
+            // compare with comparator
+            return c.compare(lhs, rhs);
+        }
     }
 
+    /**
+     * Convert a given value to index of the subtree that value belongs
+     * to.
+     * @param n The value to be converted
+     * @return  Index of the correct subtree
+     */
+    private int getIndexForValue(T n) {
+        int idx = 0;
+        while (idx < landmarkCount && compare(n, landmarks[idx]) >= 0) {
+            idx++;
+        }
+        return idx;
+    }
+
+    @SuppressWarnings("unchecked")
     public TwoThreeFourSet() {
-        
+        subtrees = new TwoThreeFourSet[4];
+        landmarks = (T[]) new Object[3];
+        landmarkCount = 0;
+    }
+
+    public TwoThreeFourSet(Comparator<T> ca) {
+        this();
+        c = ca;
     }
 
     @Override
     public boolean add(T elem) {
-        // TODO Create add method 
+        // TODO add method
+        if (contains(elem)) return false;
+
+        // element hast to be inserted.
+        
+        
+
+
         return true;
     };
 
     @Override
+    @SuppressWarnings("unchecked")
     public T get(T elem) {
-        // TODO Create get method
-        return null;
+        for (int i = 0; i < landmarkCount; ++i) {
+            if (compare(elem, landmarks[i]) == 0) {
+                return landmarks[i];
+            }
+        }
+
+        int idx = getIndexForValue(elem);
+        TwoThreeFourSet subtree = subtrees[idx];
+    
+        return subtree == null ? null: (T) subtree.get(elem);
     }
 
     @Override
     public boolean contains(T elem) {
-        // TODO create contains method
-        return true;
+        return get(elem) != null;
     }
 
     @Override
     public int size() {
-        // TODO Create size method
-        return 0;
+        int n = landmarkCount;
+        for (int i = 0; i <= landmarkCount; ++i) {
+            TwoThreeFourSet subtree = subtrees[i];
+            if (subtree != null) {
+                n += subtree.size();
+            }
+        }
+        return n;
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public T first() {
-        // TODO Create first method
-        return null;
+        TwoThreeFourSet subtree = subtrees[0];
+        if (subtree != null) {
+            return (T) subtree.first();
+        } else {
+            return landmarks[0];
+        }
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public T last() {
-        // TODO Create last method
-        return null;
+        TwoThreeFourSet subtree = subtrees[landmarkCount];
+        if (subtree != null) {
+            return (T) subtree.last();
+        } else {
+            return landmarks[landmarkCount - 1];
+        }
     } 
 
     @Override
     public Comparator<T> comparator() {
-        // TODO Create comparator method
-        return null;
+        return c;
     };
 
     @Override
@@ -83,8 +162,14 @@ public class TwoThreeFourSet<T> implements SortedTreeSet<T> {
 
     @Override
     public int height() {
-        // TODO Create height method
-        return 0;
+        int max = -1;
+        for (int i = 0; i < landmarkCount; ++i) {
+            TwoThreeFourSet subtree = subtrees[i];
+            if (subtree != null) {
+                max = Math.max(max, subtree.height());
+            }
+        }
+        return max + 1;
     }
 
 }
