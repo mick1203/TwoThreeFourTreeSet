cba743d834f67511a729eb215f34dabe5e006d38
Michael Burgstaller <trial@michaelburgstaller.com>
2018-05-06 23:29:34 +0200

    Created main program that outputs a beautifully styled tree of a specified size

--- src/two-three-four-set/src/swe4/collections/Main.java
+++ src/two-three-four-set/src/swe4/collections/Main.java
@@ -3,7 +3,16 @@ package swe4.collections;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        if (args.length != 1) return;
+
+        int count = Integer.parseInt(args[0]);
+        TwoThreeFourTreeSet<Integer> set = new TwoThreeFourTreeSet<>();
+
+        for (int i = 0; i < count; ++i) {
+            set.add(i);
+        }
+
+        System.out.println(set.toBeautifulString());
     }
 
 }
